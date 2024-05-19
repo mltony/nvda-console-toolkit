7abbc994972144ca60bd10d4fc2004f50156f8f5
mltony <anton.malykh+0@gmail.com>
2024-05-19 13:19:11 -0700

    Bump version

--- buildVars.py
+++ buildVars.py
@@ -19,7 +19,7 @@ addon_info = {
 	# Translators: Long description to be shown for this add-on on add-on information from add-ons manager
 	"addon_description" : _("""Accessibility improvements for Windows console."""),
 	# version
-	"addon_version" : "1.2",
+	"addon_version" : "1.3",
 	# Author(s)
 	"addon_author" : u"Tony Malykh <anton.malykh@gmail.com>",
 	# URL for the add-on documentation support
@@ -29,7 +29,7 @@ addon_info = {
 	# Minimum NVDA version supported (e.g. "2018.3")
 	"addon_minimumNVDAVersion" : "2019.3.0",
 	# Last NVDA version supported/tested (e.g. "2018.4", ideally more recent than minimum version)
-	"addon_lastTestedNVDAVersion" : "2023.1",
+	"addon_lastTestedNVDAVersion" : "2024.1",
 	# Add-on update channel (default is stable or None)
 	"addon_updateChannel" : None,
 }
